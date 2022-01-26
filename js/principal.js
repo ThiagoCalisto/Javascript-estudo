ada50e8e20eb34ad6af5718f68531b021fdf3571
ThiagoCalisto <51367004+ThiagoCalisto@users.noreply.github.com>
2022-01-26 17:51:46 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,16 @@
+console.log("sucess load")
+
+let titulo = document.querySelector(".titulo");
+
+titulo.textContent = "banana";
+
+/* let first = document.querySelector("#first"); */
+
+
+console.log(document.querySelector("#first"))
+
+let altura = first.querySelector(".info-altura").textContent;
+let peso = first.querySelector(".info-peso").textContent;
+
+console.log(altura)
+console.log(peso)
