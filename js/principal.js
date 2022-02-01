607a3f1f03bb5dab022c8a0d2329741873affa0a
ThiagoCalisto <51367004+ThiagoCalisto@users.noreply.github.com>
2022-02-01 17:16:53 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -7,10 +7,18 @@ titulo.textContent = "banana";
 /* let first = document.querySelector("#first"); */
 
 
-console.log(document.querySelector("#first"))
-
 let altura = first.querySelector(".info-altura").textContent;
 let peso = first.querySelector(".info-peso").textContent;
 
+let tdImc = first.querySelector(".info-imc");
+
+
+let imc = peso / (altura * altura);
+
+
+
 console.log(altura)
 console.log(peso)
+
+tdImc.textContent = imc;
+
