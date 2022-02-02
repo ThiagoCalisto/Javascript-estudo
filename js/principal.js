7720fab4e29e14fcb384408a0260ebfd9761bde7
warlocalgames@gmail.com <warlocalgames@gmail.com>
2022-02-02 17:28:06 -0300

    att

--- js/principal.js
+++ js/principal.js
@@ -7,10 +7,37 @@ titulo.textContent = "banana";
 /* let first = document.querySelector("#first"); */
 
 
-console.log(document.querySelector("#first"))
-
 let altura = first.querySelector(".info-altura").textContent;
 let peso = first.querySelector(".info-peso").textContent;
 
-console.log(altura)
-console.log(peso)
+let tdImc = first.querySelector(".info-imc");
+
+let pesoValido = true;
+let alturaValida = true;
+
+if (peso < 0 || peso > 1000){
+    console.log("Peso invalido!");
+    pesoValido = false
+    tdImc.textContent = "Peso invalido!";
+}
+
+if (altura < 0 || altura > 3.00){
+    console.log("Altura invalida!");
+    alturaValida = false;
+    tdImc.textContent = "Altura invalida!";
+}
+
+if (alturaValida && pesoValido){
+
+    let imc = peso / (altura * altura);
+
+    console.log(altura)
+    console.log(peso)
+
+    tdImc.textContent = imc;
+
+}
+
+
+
+
